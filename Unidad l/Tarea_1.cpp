b5c773f16b5a5e385cda9cc69da03eb903c1abc9
Denia09 <106629289+Denia09@users.noreply.github.com>
2022-06-01 17:35:46 -0600

    Add files via upload

---
+++ Unidad l/Tarea_1.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+
+
+using namespace std;
+
+int main(int argc, char** argv) {
+	
+
+	int a=0;
+	int b=0;
+	int  suma=0;
+	int  resta=0;
+	int  multiplicacion=0;
+	int division=0;
+	
+	cout << "Introduzca el valor de a " ;
+	cin >> a;
+	
+	cout<< "\nIntroduzca el valor de b "; 
+	cin >> b;
+	
+	
+	suma= a+b;
+	resta= a-b;
+	multiplicacion= a*b;
+	division= a/b;
+	
+	
+	cout<< "\nLa suma de a y b es: " << suma << endl;
+	cout<< "La resta de a y b es: " << resta << endl;
+	cout<< "La multiplicacion de a y b es: " << multiplicacion<< endl;
+	cout<< "La division de a y b es: " << division << endl;
+	
+	
+	return 0;
+}
