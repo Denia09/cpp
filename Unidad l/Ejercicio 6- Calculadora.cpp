1de705266ca647f35c8241b096a702bd5e40292f
Denia09 <106629289+Denia09@users.noreply.github.com>
2022-06-01 19:59:28 -0600

    Update Ejercicio 6- Calculadora.cpp

--- Unidad l/Ejercicio 6- Calculadora.cpp
+++ Unidad l/Ejercicio 6- Calculadora.cpp
@@ -7,13 +7,13 @@ using namespace std;
 
 int main(int argc, char** argv) {
 	
-
+        //Entradas
 	int a=0;
 	int b=0;
 	int  suma=0;
 	int  resta=0;
-	int  multiplicacion=0;
-	int division=0;
+	int  multip=0;
+	int div=0;
 	
 	cout << "Introduzca el valor de a " ;
 	cin >> a;
@@ -21,17 +21,17 @@ int main(int argc, char** argv) {
 	cout<< "\nIntroduzca el valor de b "; 
 	cin >> b;
 	
-	
+	//Proceso
 	suma= a+b;
 	resta= a-b;
-	multiplicacion= a*b;
-	division= a/b;
+	multip= a*b;
+	div= a/b;
 	
-	
+	//Salida
 	cout<< "\nLa suma de a y b es: " << suma << endl;
 	cout<< "La resta de a y b es: " << resta << endl;
-	cout<< "La multiplicacion de a y b es: " << multiplicacion<< endl;
-	cout<< "La division de a y b es: " << division << endl;
+	cout<< "La multiplicacion de a y b es: " << multip<< endl;
+	cout<< "La division de a y b es: " << div << endl;
 	
 	
 	return 0;
