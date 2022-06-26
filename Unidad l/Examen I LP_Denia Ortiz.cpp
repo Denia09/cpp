cb0310e441ed2df00706016d22d91c678b7f2401
Denia09 <106629289+Denia09@users.noreply.github.com>
2022-06-26 11:51:57 -0600

    Add files via upload

---
+++ Unidad l/Examen I LP_Denia Ortiz.cpp
@@ -0,0 +1,126 @@
+/*
+
+========
+
+main.cpp
+
+========
+
+*/
+
+#include <windows.h>
+#include <iostream>
+using namespace std;
+
+int Modulus(int iN, int iMod) {
+	int iQ = (iN/iMod);
+	return iN - (iQ*iMod);
+}
+
+char GetChar(int iGenerator, char cBase, int iRange) {
+	return (cBase + Modulus(iGenerator, iRange));
+}
+
+int main() {
+	string constrasena;
+	
+	
+	cout << "Eres un agente? ingresa tu contrasena ";
+	cin >> constrasena;
+	
+	if (constrasena == "smith") {
+		system("COLOR 02");
+
+		char caRow[80];
+		int j = 7;
+		int k = 2;
+		int l = 5;
+		int m = 1;
+
+		while (true) {
+			int i = 0;
+			
+			// caracteres aleatorios
+			while (i < 80) {
+				if (caRow[i] != ' ') {
+					caRow[i] = GetChar(j + i*i, 33, 30);
+				}
+				std::cout << caRow[i];
+				++i;
+			}
+			j = (j + 31);
+			k = (k + 17);
+			l = (l + 47);
+			m = (m + 67);
+			caRow[Modulus(j, 80)] = '-';
+			caRow[Modulus(k, 80)] = ' ';
+			caRow[Modulus(l, 80)] = '-';
+			caRow[Modulus(m, 80)] = ' ';
+			
+			// incremente el valor de 3000000 para retrasar el proceso
+			i = 0;
+			while (i < 300000) {
+				GetChar(1, 1, 1);
+				 ++i;
+			}
+		}		
+	} 
+	else if (constrasena == "neo") {
+		system("COLOR 02");
+
+		char caRow[80];
+		int j = 7;
+		int k = 2;
+		int l = 5;
+		int m = 1;
+		int contador=1;
+
+		while (true) {
+			int i = 0;
+			
+			// caracteres aleatorios
+			if(contador <= 10)
+			{
+			while (i < 80) {
+				if (caRow[i] != ' ') {
+					caRow[i] = GetChar(j + i*i, 33, 30);
+				}
+				std::cout << caRow[i];
+				++i;
+			}
+			j = (j + 31);
+			k = (k + 17);
+			l = (l + 47);
+			m = (m + 67);
+			caRow[Modulus(j, 80)] = '-';
+			caRow[Modulus(k, 80)] = ' ';
+			caRow[Modulus(l, 80)] = '-';
+			caRow[Modulus(m, 80)] = ' ';
+			
+			// incremente el valor de 3000000 para retrasar el proceso
+			i = 0;
+			while (i < 300000) {
+				GetChar(1, 1, 1);
+				 ++i;	 
+				}
+			contador++;		
+		}
+		else 
+			{	
+				cout<<endl;
+
+				cout<<"\nSistema FALLO";
+				system("COLOR 05");
+				Beep(1568,300);
+				Beep(1568,300);
+				Beep(1568,300);
+				break;
+			}
+		}		
+	} 
+	else {
+		cout << "acceso a la Matrix DENEGADO";
+	}
+
+    return 0;
+}
